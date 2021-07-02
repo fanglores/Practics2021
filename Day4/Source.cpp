8992f642c8caade15966acf607a3234575870d52
Konstantin <72685417+fanglores@users.noreply.github.com>
2021-07-02 12:54:09 +0300

    Update Source.cpp
    Added Image struct half functional (save image is left). All tests are operational. Everything works.

--- Day4/Source.cpp
+++ Day4/Source.cpp
@@ -28,19 +28,107 @@ struct Image {
 	int* pixels;
 };
 
-
-int setPointImage(Image* img, int x, int y)
+void createImage(Image* img, int tmx, int tmy)
 {
-	return img->pixels[(y - 1) * img->mx + x - 1];
+	img->mx = tmx;
+	img->my = tmy;
+	img->pixels = new int[tmy * tmx]{ 0 };
 }
 
-void getPointImage(Image* img, int x, int y, int color)
+void setPointImage(Image* img, int x, int y, int color)
 {
 	img->pixels[(y - 1) * img->mx + x - 1] = color;
 }
 
-int main()
+int getPointImage(Image* img, int x, int y)
+{
+	return img->pixels[(y - 1) * img->mx + x - 1];
+}
+
+void showImage(Image* img)
+{
+	for (int i = 0; i < img->my; i++)
+	{
+		for (int j = 0; j < img->mx; j++)
+		{
+			cout << img->pixels[i * img->mx + j] << " ";
+			//color output?
+		}
+		cout << endl;
+	}
+}
+/*
+void saveImage(Image* img, char& filename)
+{
+	FILE* out;
+	out = fopen_s(filename, "xz");	//color format?
+	fprintf(out, "P6\n");
+	fprintf(out, "%d %d\n", img->mx, img->my);
+	fprintf(out, "%d\n", 255);
+	fwrite(img->pixels, 3 * img->mx, img->my, out);
+
+	fclose(out);
+}
+*/
+
+bool testCreateImage()
+{
+	Image* im = new Image;
+	createImage(im, 2, 5);
+
+	if (im->pixels[0] == 0 && im->pixels[9] == 0 && im->mx == 2 && im->my == 5)
+		return true;
+
+	return false;
+}
+
+bool testGetPointImage()
+{
+	Image* im = new Image;
+	createImage(im, 2, 5);
+	im->pixels[9] = 255;
+
+	if (getPointImage(im, 2, 5) == 255 && getPointImage(im, 1, 4) == 0)
+		return true;
+
+	return false;
+}
+
+bool testSetPointImage()
+{
+	Image* im = new Image;
+	createImage(im, 2, 5);
+	setPointImage(im, 1, 3, 127);
+	setPointImage(im, 2, 5, 255);
+
+	if (getPointImage(im, 1, 3) == 127 && getPointImage(im, 2, 5) == 255)
+		return true;
+
+	return false;
+}
+
+
+
+bool testSaveImage()
 {
 
+
+	return false;
+}
+
+void runTest(bool (*testFunction)(), const string& testName)
+{
+	if (testFunction())
+		cout << "Test " << testName << " - OK" << endl;
+	else
+		cout << "Test " << testName << " - FAIL" << endl;
+}
+
+int main()
+{
+	Image* img_ptr = new Image;
+	runTest(testCreateImage, "CreateImage");
+	runTest(testGetPointImage, "GetPointImage");
+	runTest(testSetPointImage, "SetPointImage");
 
 }
