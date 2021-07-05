835b5e2438ee82ece2f8e1cc8522f2b779e5d5c0
Konstantin <72685417+fanglores@users.noreply.github.com>
2021-07-05 16:20:11 +0300

    Update SFML_source.cpp
    Completed version

--- Day5/SFML_source.cpp
+++ Day5/SFML_source.cpp
@@ -1,36 +1,54 @@
 #include <SFML/Graphics.hpp>
 
+/*
+Задание 12
+Разработать при помощи библиотеки SFML минимальную программу, которая будет позволять
+Запускать изменение фигуры на экран по нажатию одной клавиши и останавливать по другой
+
+Вариант 1
+Вращаем прямоугольник
+*/
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 450), "SFML works!");
 
-	int i = 0;
-	int x = 100, y = 50;
+	const int x = 100, y = 50;
+	bool rot_f = false;
 
+	//rectangle initiation
+	sf::RectangleShape rect(sf::Vector2f(x, y));
+	rect.setFillColor(sf::Color::Green);
+	rect.setPosition(2 * x, 3 * y);
 
 	while (window.isOpen())
 	{
-		sf::RectangleShape rect(sf::Vector2f(x, y));
-		rect.setFillColor(sf::Color::Green);
-		rect.setPosition(2*x, 2*y);
-		rect.rotate(i);
+		window.clear();
+		window.draw(rect);
+		window.display();
 
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
+			{
 				window.close();
+				break;
+			}
+
+			//right and left arrows control movement
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::Right) rot_f = true;
+				if (event.key.code == sf::Keyboard::Left) rot_f = false;
+				break;
+			}
 		}
 
-		window.clear();
+		if (rot_f) rect.rotate(1);
 
-		window.draw(rect);
-
-		window.display();
-
-		i++;
-
-		if (i == 360) i = 0;
+		//little smooth for the movement
+		sf::sleep(sf::milliseconds(5));
 	}
 
 	return 0;
