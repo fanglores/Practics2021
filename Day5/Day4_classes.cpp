c96a0d272590880ba39fc2820db15bd7f4ca6bbd
Konstantin <72685417+fanglores@users.noreply.github.com>
2021-07-05 10:07:09 +0300

    Update Day4_classes.cpp
    Final version

--- Day5/Day4_classes.cpp
+++ Day5/Day4_classes.cpp
@@ -16,117 +16,74 @@ int getPointImage(Image* img, int x, int y);
 void saveToPnmFile(Image* img, string fileName);
 */
 
-struct Image {
-	int mx = 0;
-	int my = 0;
-	int* pixels;	
+class Image 
+{
+private:
+	int mx;
+	int my;
+	int* pixels;
+
+public:
+	Image() 
+	{
+		mx = 0;
+		my = 0;
+	}
+
+	Image(int tmx, int tmy)
+	{
+		mx = tmx;
+		my = tmy;
+		pixels = new int[tmx * tmy]{ 1 };
+	}
+
+	void setPointImage(int x, int y, int color)
+	{
+		pixels[(y * mx + x)] = color;
+	}
+
+	int getPointImage(int x, int y)
+	{
+		return pixels[y * mx + x];
+	}
+
+	void showImage()
+	{
+		for (int i = 0; i < my; i++)
+		{
+			for (int j = 0; j < mx; j++)
+				cout << pixels[i * mx + j] << " ";
+
+			cout << endl;
+		}
+	}
+
+	void saveImage(const string filename)
+	{
+		fstream out;
+		out.open(filename, fstream::out);
+		out << "P1" << endl;
+		out << mx << " " << my << endl;
+
+		for (int i = 0; i < my; i++)
+		{
+			for (int j = 0; j < mx; j++)
+				out << pixels[i * mx + j] << " ";
+			
+			out << endl;
+		}
+
+		out.close();
+	}
 };
 
-void createImage(Image* img, int tmx, int tmy)
-{
-	img->mx = tmx;
-	img->my = tmy;
-	img->pixels = new int[tmx * tmy]{ 1 };
-}
-
-void setPointImage(Image* img, int x, int y, int color)
-{
-	img->pixels[(y * img->mx + x)] = color;
-}
-
-int getPointImage(Image* img, int x, int y)
-{
-	return img->pixels[y * img->mx + x];
-}
-
-void showImage(Image* img)
-{
-	for (int i = 0; i < img->my; i++)
-	{
-		for (int j = 0; j < img->mx; j++)
-		{
-			cout << img->pixels[i * img->mx + j] << " ";
-			//color output?
-		}
-		cout << endl;
-	}
-}
-
-void saveImage(Image* img, const string filename)
-{
-	fstream out;
-	out.open(filename, fstream::out);
-	out << "P1" << endl;
-	out << img->mx << " " << img->my << endl;
-
-	for (int i = 0; i < img->my; i++)
-	{
-		for (int j = 0; j < img->mx; j++)
-		{
-			out << img->pixels[i * img->mx + j] << " ";
-		}
-		out << endl;
-	}
-
-	out.close();
-}
-
-
-bool testCreateImage()
-{
-	Image* im = new Image;
-	createImage(im, 2, 5);
-
-	if (im->pixels[0] == 0 && im->pixels[9] == 0 && im->mx == 2 && im->my == 5)
-		return true;
-
-	return false;
-}
-
-bool testGetPointImage()
-{
-	Image* im = new Image;
-	createImage(im, 2, 5);
-	im->pixels[9] = 255;
-
-	if (getPointImage(im, 1, 4) == 255 && getPointImage(im, 1, 2) == 0)
-		return true;
-
-	return false;
-}
-
-bool testSetPointImage()
-{
-	Image* im = new Image;
-	createImage(im, 2, 5);
-	setPointImage(im, 1, 0, 127);
-	setPointImage(im, 1, 4, 255);
-
-	if (getPointImage(im, 1, 0) == 127 && getPointImage(im, 1, 4) == 255)
-		return true;
-	
-	return false;
-}
-
-void runTest(bool (*testFunction)(), const string& testName)
-{
-	if (testFunction())
-		cout << "Test " << testName << " - OK" << endl;
-	else
-		cout << "Test " << testName << " - FAIL" << endl;
-}
 
 int main()
 {
-	runTest(testCreateImage, "CreateImage");
-	runTest(testGetPointImage, "GetPointImage");
-	runTest(testSetPointImage, "SetPointImage");
-
-	Image* img_ptr = new Image;
-	createImage(img_ptr, 3, 3);
-	setPointImage(img_ptr, 0, 0, 1);
-	setPointImage(img_ptr, 1, 1, 1);
-	setPointImage(img_ptr, 2, 2, 1);
-	showImage(img_ptr);
-	saveImage(img_ptr, "hello_image.pnm");
+	Image img(3, 3);
+	img.setPointImage(0, 0, 1);
+	img.setPointImage(1, 1, 1);
+	img.setPointImage(2, 2, 1);
+	img.showImage();
+	img.saveImage("hello_image.pnm");
 }
